ae95f0a7ef2482c67df49cb8e02828ab1ace1876
sharifabee <sharifabee.alur@gmail.com>
2024-12-13 09:38:28 +0530

    dec13 p1

--- Demo3.java
+++ Demo3.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Demo3 {
 
+	// RaMu
+	// rAmU
 	public static void main(String[] args) {
+		
+		
+		
+		
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter name");
 		String name = sc.next();
